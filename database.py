8d279aa7e91e8df585e05a8e9919d2017a3b476f
Seth Morgan <seth@sethmorgan.io>
2024-07-19 12:13:30 -0700

    cast port number to int

--- database.py
+++ database.py
@@ -6,7 +6,7 @@ db_host = os.environ["DB_HOST"]
 db_pass = os.environ["DB_PASS"]
 db_user = os.environ["DB_USER"]
 db_name = os.environ["DB_DB"]
-db_port = os.environ["DB_PORT"]
+db_port = int(os.environ["DB_PORT"])
 
 db = MySQLDatabase(None)
 db.init(database=db_name, host=db_host, user=db_user, port=db_port, password=db_pass)
