88b70f85039e49578b6148b676cd93038eec46db
Seth Morgan <seth@sethmorgan.io>
2024-07-19 11:36:27 -0700

    add include_router statement

--- app.py
+++ app.py
@@ -22,8 +22,8 @@ app = FastAPI(
     summary="The MLR-Reference API allows users to access league data (players, games, teams, etc.) from the MLR database.",
     description="For issues, please contact Sterling Turlington on the MLR Main Discord (@veritablequandary).",
     version="0.0.1",
-    docs_url=None,
-    redoc_url="/",
+    docs_url="/",
+    redoc_url=None,
     openapi_tags=tag_definitions,
 )
 
@@ -42,3 +42,6 @@ async def get_all_batting_types():
     battingTypes = BattingType.get().order_by(BattingType.type)
     if len(battingTypes) == 0:
         raise HTTPException(status_code=404, detail="No batting types found.")
+
+
+app.include_router(dataRouter)
