5b44a901c03226c8f9165a2ee5b7945df65ffad5
Seth Morgan <seth@sethmorgan.io>
2024-08-08 10:47:19 -0700

    fix requirements

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = FastAPI(
     title="MLR-Reference API",
     summary="The MLR-Reference API allows users to access read-only data from the MLR database.",
     description="For issues, please contact Sterling Turlington on the MLR Main Discord (@veritablequandary).",
-    version="1.0.0",
-    docs_url="/",
-    redoc_url=None,
+    version="0.1.0",
+    docs_url=None,
+    redoc_url="/",
 )
