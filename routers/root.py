2bb08b37623317d1660491c0be81b6ca753023e8
Seth Morgan <seth@sethmorgan.io>
2024-08-08 11:31:09 -0700

    add typing for hello response

--- routers/root.py
+++ routers/root.py
@@ -1,11 +1,14 @@
 from fastapi import APIRouter
+from models.hello import HelloResponse
 
 
 root_router = APIRouter(tags=["/"])
 
 
 # Since health check must be successful before the API becomes active, no failure responses need to be handled
-@root_router.get("hello", name="hello", summary="Check API status")
+@root_router.get(
+    "hello", name="hello", summary="Check API status", response_model=HelloResponse
+)
 def hello():
     """
     Use this endpoint to check whether the API is active.
