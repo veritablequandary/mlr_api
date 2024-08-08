2eabf55e261b887b6b3310357166abe72669271c
Seth Morgan <seth@sethmorgan.io>
2024-08-08 11:26:33 -0700

    add title to hello endpoint docs

--- routers/root.py
+++ routers/root.py
@@ -5,7 +5,7 @@ root_router = APIRouter(tags=["/"])
 
 
 # Since health check must be successful before the API becomes active, no failure responses need to be handled
-@root_router.get("/hello", summary="Check API status")
+@root_router.get("hello", name="hello", summary="Check API status")
 def hello():
     """
     Use this endpoint to check whether the API is active.
